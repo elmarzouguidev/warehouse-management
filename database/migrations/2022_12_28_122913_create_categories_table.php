61be8115a7ecfb495b4cbe0b41054a1cb779cb46
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2022-12-28 13:32:29 +0100

    ovveride laravel default Stub

--- database/migrations/2022_12_28_122913_create_categories_table.php
+++ database/migrations/2022_12_28_122913_create_categories_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+
+            $table->boolean('is_active')->default(true); 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
