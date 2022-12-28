fb3822a955c00c545713e96e2e1fdb06705bca5e
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2022-12-28 14:04:38 +0100

    setup livewire

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="googlebot" content="noindex,nofollow">
+    <meta name="google" content="notranslate">
+    <link rel="dns-prefetch" href="{{ request()->getSchemeAndHttpHost() }}">
+    <link rel="preconnect" href="{{ request()->getSchemeAndHttpHost() }}">
+    <link rel="prefetch" href="{{ request()->getSchemeAndHttpHost() }}">
+    <link rel="prerender" href="{{ request()->getSchemeAndHttpHost() }}">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Warehouse management</title>
+
+    @livewireStyles
+</head>
+
+<body>
+
+    @livewireScripts
+</body>
+
+</html>
