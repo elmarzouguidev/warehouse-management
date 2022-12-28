142df029987b918f220d40bf24b17cda89bd0bb2
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2022-12-28 14:30:40 +0100

    update same configurations files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->uuid();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
+            $table->boolean('is_active')->default(true);
+    
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
